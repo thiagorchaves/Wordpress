9dd2136284af536d617bbf68914299564224621e
thiagorchaves <thiago.romualdo@gmail.com>
2017-11-13 18:03:33 -0200

    wordpress

--- wp-config.php
+++ wp-config.php
@@ -27,7 +27,7 @@ define('DB_NAME', 'testedb');
 define('DB_USER', 'root');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'teste');
 
 /** Nome do host do MySQL */
 define('DB_HOST', 'localhost');
